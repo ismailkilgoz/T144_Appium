8b268ad59b77bf41899e60a4df8e3f0bf60c4fb4
Ismail KILGOZ <ligerhyme@hotmail.com>
2024-10-27 18:39:16 +0300

    son

--- src/test/java/APK_Yukleme.java
+++ src/test/java/APK_Yukleme.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+import utilities.Driver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class APK_Yukleme {
 
-    AndroidDriver<AndroidElement>driver;
+    AndroidDriver<AndroidElement>androidDriver= Driver.getAndroidDriver();
 
     @Test
     public void apkSetup() throws MalformedURLException {
@@ -23,7 +24,7 @@ public class APK_Yukleme {
                 "C:\\Users\\Administrator\\IdeaProjects\\Appium_144\\App\\all-currency-converter-3-9-0 (1).apk");
 
 
-        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        androidDriver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        androidDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 }
